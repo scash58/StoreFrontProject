cb1fc15f31f88c624fc02fe6e83ae378ffc4c41a
Scott Cashion <scashion58@outlook.com>
2022-01-19 10:16:52 -0600

    Bundled all styling in the BundleConfig.cs and added Styles.Render in the Header and Footers of the _Layout.

--- StoreFrontProject.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFrontProject.UI.MVC/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@ namespace StoreFrontProject.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,13 +18,34 @@ namespace StoreFrontProject.UI.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(
+                        "~/Content/assets/js/jquery-2.1.0.min.js",
+                        "~/Content/assets/js/popper.js",
+                        "~/Content/assets/js/bootstrap.min.js",
+                        "~/Content/assets/js/owl-carousel.js",
+                        "~/Content/assets/js/accordions.js",
+                        "~/Content/assets/js/datepicker.js",
+                        "~/Content/assets/js/scrollreveal.min.js",
+                        "~/Content/assets/js/waypoints.min.js",
+                        "~/Content/assets/js/jquery.counterup.min.js",
+                        "~/Content/assets/js/imgfix.min.js",
+                        "~/Content/assets/js/slick.js",
+                        "~/Content/assets/js/lightbox.js",
+                        "~/Content/assets/js/isotope.js",
+                        "~/Content/assets/js/custom.js"
+                        ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/assets/css/bootstrap.min.css",
+                        "~/Content/assets/css/font-awesome.css",
+                        "~/Content/assets/css/templatemo-hexashop.css",
+                        "~/Content/assets/css/owl-carousel.css",
+                        "~/Content/assets/css/lightbox.css"
+                      ));
         }
     }
 }
